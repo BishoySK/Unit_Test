f493cef33df285f4756767b8d725a7017b02201c
bishoy <bishoy.saberibrahim@gmail.com>
2023-02-22 20:13:55 +0200

    first commit

---
+++ UnitTestApp/SampleParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestApp
+{
+    public class SampleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                throw new InvalidOperationException("I can only handle 0 or 1 numbers for now!");
+            }
+        }
+
+        public string NoRepeat(string target, char ch)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (target[i] != ch)
+                    result.Append(target[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ UnitTestApp_Test/SimpleParserTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestApp;
+
+namespace UnitTestApp_Test
+{
+    [TestFixture]
+    public class SimpleParserTest
+    {
+        [Test]
+        public void ParseAndSum_OneNumber_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+           var result= sampleParser.ParseAndSum("5");
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void ParseAndSum_Zero_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+            var result = sampleParser.ParseAndSum("0");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ParseAndSum_MoreThanOneNumber_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+            Assert.That(()=>sampleParser.ParseAndSum("5,8"), Throws.TypeOf<InvalidOperationException>());
+        }
+
+
+        [Test]
+        public void NoRepeat_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+            var result = sampleParser.NoRepeat("Hello", 'z');
+
+            Assert.That(result, Is.EqualTo("Hello"));
+        }
+
+
+        [Test]
+        public void NoRepeat_RepeatOneChar_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+            var result = sampleParser.NoRepeat("Hello", 'e');
+
+            Assert.That(result, Is.EqualTo("Hllo"));
+        }
+
+        [Test]
+        public void NoRepeat_RepeatTwoChar_Test()
+        {
+            SampleParser sampleParser = new SampleParser();
+
+            var result = sampleParser.NoRepeat("Hello", 'l');
+
+            Assert.That(result, Is.EqualTo("Heo"));
+        }
+    }
+}
